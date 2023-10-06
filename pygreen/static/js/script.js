34b07893826cad859de09bccee4646f4d333d695
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2023-10-06 11:25:29 -0300

    adicionando responsividade
    responsividade no hero do home, ** falta adicionar a área de galeria e de trailer. **

--- pygreen/static/js/script.js
+++ pygreen/static/js/script.js
@@ -22,3 +22,14 @@ closeLoginButton.addEventListener("click", function() {
         document.querySelector("#login-screen").classList("fade-out")
     }
 });
+
+function showMenu() {
+    let menuMobile = document.querySelector('.menu-mobile');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        // document.querySelector('.icon') = <i class="fa-solid fa-bars"></i>;
+    } else {
+        menuMobile.classList.add('open');
+        // document.querySelector('.icon')= <i class="fa-solid fa-xmark"></i>;
+    }
+}
