1827cd83a0ff8315c3e194eaede3a5d28607eb1c
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2023-11-12 14:21:21 -0300

    da pra clicar no botao de login

--- pygreen/static/js/script.js
+++ pygreen/static/js/script.js
@@ -75,7 +75,6 @@ function showMenu() {
     }
 }
 
-<<<<<<< HEAD
 var formProfile = document.getElementById('form-profile')
 var subProfile = document.getElementById('sub-title-profile')
 var formSignup = document.getElementById('form-signup')
@@ -90,63 +89,6 @@ function swapForm() {
 }
 
 btnSignup.addEventListener('click', swapForm)
-=======
-
-var inputLogin = document.querySelector(".password");
-var eye = document.querySelector(".fa-eye");
-var eyeSlash = document.querySelector(".fa-eye-slash");
-var inputSignup = document.querySelector(".password-signup")
-var eye2 = document.querySelector(".signup1");
-var eyeSlash2 = document.querySelector(".signup2");
-
-function showPassword() {
-    if(eye.classList.contains('open')) {
-        eye.classList.remove('open')
-        eye.classList.add('close')
-        inputLogin.type = 'text';
-    }
-    else {
-        eye.classList.add('open')
-        eye.classList.remove('close')
-        inputLogin.type = 'password';
-    }
-
-    if(eyeSlash.classList.contains('open')) {
-        eyeSlash.classList.remove('open')
-        eyeSlash.classList.add('close')
-        inputLogin.type = 'password';
-    }
-    else {
-        eyeSlash.classList.add('open')
-        eyeSlash.classList.remove('close')
-        inputLogin.type = 'text';
-    }
-}
-
-function showPassword() {
-    if(eye2.classList.contains('open')) {
-        eye2.classList.remove('open')
-        eye2.classList.add('close')
-        inputSignup.type = 'text';
-    }
-    else {
-        eye2.classList.add('open')
-        eye2.classList.remove('close')
-        inputSignup.type = 'password';
-    }
-
-    if(eyeSlash2.classList.contains('open')) {
-        eyeSlash2.classList.remove('open')
-        eyeSlash2.classList.add('close')
-        inputSignup.type = 'password';
-    }
-    else {
-        eyeSlash2.classList.add('open')
-        eyeSlash2.classList.remove('close')
-        inputSignup.type = 'text';
-    }
-}
->>>>>>> 9bff9df2459afef70e44e447e5c0a7752980b8ba
 
 
 
